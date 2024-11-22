93c9452da2c6b24aa61f716f5d2af02906fe7996
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2024-11-22 21:49:50 +0600

    Time: 0 ms (100.00%) | Memory: 8.7 MB (6.43%) - LeetSync

---
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumChairs(string s) {
+        int chair = 0 , maxChair = 0;
+        for(char c : s){
+            if(c == 'E')
+                chair++;
+            else chair--;
+            maxChair = max(maxChair , chair);
+        }
+        return maxChair;
+    }
+};
