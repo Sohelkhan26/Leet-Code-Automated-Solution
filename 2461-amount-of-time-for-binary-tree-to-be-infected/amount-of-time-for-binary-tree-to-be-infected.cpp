b1d4af9876c7840ffffc5bc06e0ddd9487d59138
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2024-12-01 14:20:37 +0600

    Time: 7 ms (92.41%) | Memory: 127.9 MB (88.32%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,53 +1,47 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int amountOfTime(TreeNode* root, int start) {
-        int n = 1e5 + 1;
-        vector<int> adj[n];
-        function<void(TreeNode*)> createGraph = [&](TreeNode* root) -> void{
-            if(not root)
-                return;
-            if(root -> left){
-                adj[root -> val].push_back(root -> left -> val);
-                adj[root -> left -> val].push_back(root -> val);
-                createGraph(root -> left);
-            }
-            if(root -> right){
-                adj[root -> val].push_back(root -> right -> val);
-                adj[root -> right -> val].push_back(root -> val);
-                createGraph(root -> right);
-            }
-        };
-        createGraph(root);
-        vector<bool> vis(n);
-        queue <int> q;
-        q.push(start);
-        vis[start] = true;
-        int time = -1;
-        while(not q.empty()){
-            int len = q.size();
-            while(len--){
-                int node = q.front();
-                q.pop();
-                for(int nei : adj[node]){
-                    if(not vis[nei]){
-                        q.push(nei);
-                        vis[nei] = true;
-                    }
-                }
-            }
-            time++;
+    int ans = 0;
+    int solve(TreeNode* root , int start){
+        int depth = 0;
+        if(not root)
+            return depth;
+        int leftDepth = solve(root -> left , start);
+        int rightDepth = solve(root -> right , start);
+        if(root -> val == start){
+            ans = max(leftDepth , rightDepth); // এই node যদি root node হয় তাহলে এটাই এনসার। পরে এটা আর আপডেট হবে না। তা না হলে start node tree এর মাঝের কোন একটা নোড। রুট নোড থেকে start নোড এর distance জানতে হবে। -ve value return করে মানে এই পাশে start node আছে। এবং এই নেগেটিভে ভ্যালুর absolute value নির্দেশ করে কয় লেভেল পরে আছে। 
+            depth = -1;
+        }else if(min(leftDepth , rightDepth) >= 0)
+            depth = max(leftDepth , rightDepth) + 1; // মানে এই সাবট্রিতে start node নেই। 
+        else {
+            ans = max(ans , abs(leftDepth) + abs(rightDepth));
+            depth = min(leftDepth , rightDepth) - 1; // subtract one to account for each level
         }
-        return time;
+        return depth;
+    }
+    int amountOfTime(TreeNode* root, int start) {
+        solve(root , start);
+        return ans;
     }
 };
+/*
+start node যদি root node হয় তাহলে ট্রির হাইট ই এনসার। তা না হয়ে যদি মাঝখানের কোন node start হয় তাহলে এনসার হবে distance(root -> furthest leaf) + distance(root -> start) উল্লেখ্য যে, furthest leaf মানে, যে subtree তে start নেই সেই subtree এর furthest leaf.
+                            10
+                         /      \
+                        1        2
+                       /  \     /  \
+                      3    4   5    6
+                    /   \          /  \
+                   7     8        24   32
+6 যদি start node হয় তাহলে ১০ -> ৬ এর দূরত্ব + ১০ -> ৭ বা ১০ -> ৮ এর দূরত্ব এর যোগফল রিটার্ন করতে হবে। ১০ -> ৬ এর দূরত্ব কিভাবে বের হবে? ৬, ২ কে -১ রিটার্ন করবে। ২ আবার ১০ কে -১ -১ = -২ রিটার্ন করবে। abs(-2) = 2 মানে দুই লেভেল পরে start নোড আছে। -ve indicates start node
+ট্রি যদিঃ 2
+        \
+          6
+            \
+              32
+                 \
+                  100
+                     \
+                      10 
+এরকম  হতো তাহলে ans = max(leftDepth , rightDepth); এই লাইনের মাধ্যমে এনসার বের হয়ে যেতো। 
+                
+*/
