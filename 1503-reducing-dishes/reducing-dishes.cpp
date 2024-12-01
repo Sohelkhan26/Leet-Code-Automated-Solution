ee1b4286a2e266112d136086c817dc1d16b75d45
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2024-12-01 15:24:36 +0600

    Time: 0 ms (100.00%) | Memory: 13.3 MB (48.92%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int dp[501][501];
-    int solve(int index , int time , vector <int> &nums){
-        if(index == nums.size())
-            return 0;
-        if(dp[index][time] != -1)
-            return dp[index][time];
-        int take = nums[index] * time + solve(index + 1 , time + 1 , nums);
-        int dont = solve(index + 1 , time , nums);
-        return dp[index][time] = max(take , dont);
-    }
     int maxSatisfaction(vector<int>& nums) {
-        memset(dp , -1 , sizeof(dp));
-        sort(nums.begin() , nums.end());
-        return solve(0 , 1 , nums);
+        sort(nums.rbegin() , nums.rend());
+        int ans = 0 , dishes = 0;
+        for(int i : nums){
+            dishes += i;
+            if(dishes > 0)
+                ans += dishes;
+            else break;
+        }
+        return ans;
     }
 };
+/*
+Similar to kadene's algo. Only take a dish if it increases the co-efficient. How do we find out if a dish increases the co-efficient?
+মজাদার dish সবার পরে রান্না করতে হবে। তাহলে এর সাথে বেশী time গুন হয়ে বেশী answer বৃদ্ধি করবে। 
+যেসব dish এর satisfaction >= 0 সবগুলাই রান্না করতে হবে। < 0 হলে দুইটা কেসঃ
+১। এটা অবশ্যই সবার প্রথমে রান্না করতে হবে। ফলে এর পরে যেগুলা রান্না করা হবে সবগুলার সাথে এক্সট্রা ১ করে গুন হবে। এক্সট্রা গুন হওয়ার কারনে এনসার এ যা কন্ট্রিবিউট করবে তার চাইতে যদি (-ve satisfaction) ওয়ালা dish যদি বেশী বিয়োগ করে ফেলে তাহলে তো রান্না করা যাবে না। 
+-1 2 5
+এখানে -১ রান্না করা যাবে কারণ এটা রান্না করলে ans += (6) হবে এবং এটার কারনে 1 বিয়োগ হবে। তাই এট লাভজনক। কিন্তু -15 2 5 এর ক্ষেত্রে -15 রান্না করা লাভজনক না। ans += (6) ans -= 15. ans += 6 হওয়ার কারণ Everytime we add a new dish to the menu list,
+all dishes on the menu list will be cooked one time unit later,
+so the result += total satisfaction on the list (ans += dishes in my code)
+*/
