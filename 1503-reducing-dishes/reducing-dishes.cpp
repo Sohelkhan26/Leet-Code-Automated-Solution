abf052ebf1a8b4de749863ddce5754db73288826
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2024-12-01 15:04:47 +0600

    Time: 18 ms (38.15%) | Memory: 12.5 MB (49.67%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    unordered_map <string , int> dp;
+    int dp[501][501];
     int solve(int index , int time , vector <int> &nums){
         if(index == nums.size())
             return 0;
-        string key = to_string(index) + "-" + to_string(time);
-        if(dp.find(key) != dp.end())
-            return dp[key];
+        if(dp[index][time] != -1)
+            return dp[index][time];
         int take = nums[index] * time + solve(index + 1 , time + 1 , nums);
         int dont = solve(index + 1 , time , nums);
-        return dp[key] = max(take , dont);
+        return dp[index][time] = max(take , dont);
     }
     int maxSatisfaction(vector<int>& nums) {
+        memset(dp , -1 , sizeof(dp));
         sort(nums.begin() , nums.end());
         return solve(0 , 1 , nums);
     }
