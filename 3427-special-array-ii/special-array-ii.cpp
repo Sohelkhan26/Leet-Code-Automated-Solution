39bf83b24b720b2577763a932a95c521f17317bd
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2024-12-09 12:51:54 +0600

    Time: 44 ms (28.13%) | Memory: 137.5 MB (9.97%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<vector<int>> special;
-        int n = nums.size() , left , right;
-        for(left = 0 , right = 1 ; right < n ; right++){
-            if(nums[right] % 2 == nums[right - 1] % 2){
-                special.push_back({left , right - 1});
-                left = right;
-            }
+        vector<int> intervals = {0};
+        for(int inter = 0 , i = 1 ; i < nums.size() ; i++){
+            if(nums[i] % 2 == nums[i - 1] % 2)
+                inter++;
+            intervals.push_back(inter);
         }
-        special.push_back({left , right});
         vector<bool> ans;
-        for(auto it : queries){
-            int left = it[0] , right = it[1];
-            int i = upper_bound(special.begin() , special.end() , vector<int>{left , INT_MAX}) - special.begin() - 1;
-            ans.push_back(special[i][1] >= right);
-        }
+        for(auto q : queries)
+            ans.push_back(intervals[q[0]] == intervals[q[1]]);
         return ans;
     }
 };
