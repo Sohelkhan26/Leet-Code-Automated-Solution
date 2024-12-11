85aa4628ee104b716d881fea6052aefc479fee71
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2024-12-11 23:44:54 +0600

    Time: 12 ms (17.17%) | Memory: 7.8 MB (27.52%) - LeetSync

--- 2595-smallest-value-after-replacing-with-sum-of-prime-factors/smallest-value-after-replacing-with-sum-of-prime-factors.cpp
+++ 2595-smallest-value-after-replacing-with-sum-of-prime-factors/smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    int smallestValue(int n) {
-        int prevN = -1;
-        while(prevN != n){
-            prevN = n;
-            int sumOfPrimes = 0;
-            for(int divisor = 2 ; divisor < n ; divisor++)
-                while(n % divisor == 0){
-                    n /= divisor;
-                    sumOfPrimes += divisor;
-                }
-            if(n != 1)
-                sumOfPrimes += n;
-            n = sumOfPrimes;
-        }
-        return n;
+    int smallestValue(int n , int sumOfPrimes = 0) {
+        for(int divisor = 2 , m = n ; divisor <= n ; divisor++)
+            for( ; m % divisor == 0 ; m /= divisor)
+                sumOfPrimes += divisor;
+        return sumOfPrimes == n ? n : smallestValue(sumOfPrimes);
     }
 };
 
 /*
-Just simulate the process. Replace n with it's prime factors. Do this until n remains same in the next iteration.
+Edge case : n = 4
 */
