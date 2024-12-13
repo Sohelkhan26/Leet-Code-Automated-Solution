c53f6b73aea6f4b58ae9f2f5028d4a8edff08536
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2024-12-13 23:37:08 +0600

    Time: 0 ms (100.00%) | Memory: 7.9 MB (20.09%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int ans = 0 , N;
-    int dp[51][200];
-    int solve(int i , char prev){
-        if(i == N)
-            return 1;
-        if(dp[i][(int)prev] != -1)
-            return dp[i][(int)prev];
-        int ans = 0;
-        for(char c : vector<char> {'a' , 'e' , 'i' , 'o' , 'u'}){
-            if(c >= prev)   
-                ans += solve(i + 1 , c);
-        }
-        return dp[i][(int)prev] = ans;
+    int a = 1 , e = 1 , i = 1 , o = 1 , u = 1;
+    void increment(){
+        a = a + e + i + o + u;
+        e = e + i + o + u;
+        i = i + o + u;
+        o = o + u;
     }
     int countVowelStrings(int n) {
-        N = n;
-        memset(dp , -1 , sizeof(dp));
-        return solve(0 , 'a');
+        for(int i = 1 ; i < n ; i++)
+            increment();
+        return a + e + i + o + u;
     }
 };
