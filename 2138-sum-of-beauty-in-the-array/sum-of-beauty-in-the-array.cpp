9ac551ca0e3cef82b4b34039a12767fd6c84c7bc
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-15 00:24:09 +0600

    Time: 2 ms (82.70%) | Memory: 89.6 MB (84.08%) - LeetSync

--- 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.cpp
+++ 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int sumOfBeauties(vector<int>& nums) {
-        int n = nums.size();
-        vector <int> pref(n) , suff(n);
-        pref[0] = nums[0];
-        for(int i = 1 ; i < n ; i++)
-            pref[i] = max(pref[i - 1] , nums[i]);
+        int n = nums.size() , maxLeft = nums[0];
+        vector <int> suff(n);
         suff[n - 1] = nums[n - 1];
         for(int i = n - 2 ; i >= 0 ; i--)
             suff[i] = min(suff[i + 1] , nums[i]);
         int ans = 0;
         for(int i = 1 ; i < n - 1 ; i++){
-            if(pref[i - 1] < nums[i] and nums[i] < suff[i + 1]){
-                cout << pref[i - 1] << ' ' << nums[i] << ' ' << suff[i + 1] << endl;
-                ans += 2;}
+            if(maxLeft < nums[i] and nums[i] < suff[i + 1])
+                ans += 2;
             else if(nums[i - 1] < nums[i] and nums[i] < nums[i + 1])
                 ans++;
+            maxLeft = max(maxLeft , nums[i]);
         }
         return ans;
     }
