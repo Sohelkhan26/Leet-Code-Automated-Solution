5ec350f3e22547e36c80c581c6494e09bfc2f858
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-14 23:45:41 +0600

    Time: 32 ms (39.82%) | Memory: 88.2 MB (73.33%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int maximumCandies(vector<int>& candies, long long k) {
-        int low = 1 , high = *max_element(candies.begin() , candies.end()) , mid , ans = 0;
+        int low = 1 , high = 1e7 , mid , ans = 0;
+        auto allocate = [&](int candy) -> bool{
+            long long child = k;
+            for(int i : candies)
+                child -= (int)(i / candy);
+            return child <= 0;
+        };
         while(low <= high){
             mid = low + (high - low) / 2;
-            long long children = 0;
-            for(int i : candies)
-                children += (int)(i / mid);
-            if(children >= k){
+            if(allocate(mid)){
                 ans = mid;
                 low = mid + 1;
             }else high = mid - 1;
@@ -15,6 +18,3 @@ public:
         return ans;
     }
 };
-/*
-Binary search over the maximum number of candies. So mid is the possible answer. If a pile has more than mid candies, we have to split it in such a way so that we can assign same number of candies to as many children possible. If a pile has 12 candies we want to split it as 5 , 5 so that we can give it to 2 children rather than 1 chilren. So 12 -> 5 , 7 . 7 -> 5 , 2
-*/
