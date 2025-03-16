4c3973ca3bde0586c8c0955a24aa3d0f66a51cd1
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-16 13:37:14 +0600

    Time: 50 ms (59.89%) | Memory: 59.3 MB (45.76%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     long long repairCars(vector<int>& ranks, int cars) {
-        long long low = 1 , high = *min_element(ranks.begin() , ranks.end()) * 1ll * cars * cars , time , ans = 0;
+        long long low = 1 , high = 1e14 , ans = -1;
         while(low <= high){
-            time = low + (high - low) / 2;
-    // check the sum of cars cleanable by all mechanic within mid time. If it's less then cars ok.
-            long long totalCar = 0;
-            for(int i : ranks){
-                totalCar += (int)sqrt(time / i);
-            }
-            if(totalCar >= cars){
+            long long time = low + (high - low) / 2 , repairedCar = 0;
+            for(int i : ranks)
+                repairedCar += (int)sqrt(time / i);
+            if(repairedCar >= cars){
                 ans = time;
                 high = time - 1;
-            }else low = time + 1;
+            }else low = time + 1; 
         }
         return ans;
     }
