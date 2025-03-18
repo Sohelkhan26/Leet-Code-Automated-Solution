7f37263bdac178878b11fdb08a8b7b49ce46e87e
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-19 01:06:22 +0600

    Time: 69 ms (36.86%) | Memory: 12.8 MB (92.86%) - LeetSync

--- 864-image-overlap/image-overlap.cpp
+++ 864-image-overlap/image-overlap.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int solve(int xOffset , int yOffset , vector<vector<int>>& a , vector<vector<int>>& b){
-        int overlap = 0 , n = a.size();
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                int newI = i + xOffset , newJ = j + yOffset;
-                if(min(newI , newJ) >= 0 and newI < n and newJ < n)
-                    overlap += (a[i][j] == b[newI][newJ] and a[i][j]);
-            }
-        }
-        return overlap;
-    }
     int largestOverlap(vector<vector<int>>& a, vector<vector<int>>& b) {
-        int n = a.size() , ans = 0;
+        int n = a.size() , ans = 0 , overlap = 0;
         for(int xOffset = - n + 1 ; xOffset < n ; xOffset++){
             for(int yOffset = - n + 1 ; yOffset < n ; yOffset++){
-                ans = max(ans , solve(xOffset , yOffset , a , b));
+                overlap = 0;
+                for(int i = 0 ; i < n ; i++){
+                    for(int j = 0 ; j < n ; j++){
+                        int newI = i + xOffset , newJ = j + yOffset;
+                        if(min(newI , newJ) >= 0 and newI < n and newJ < n)
+                            overlap += (a[i][j] == b[newI][newJ] and a[i][j]);
+                    }
+                    ans = max(ans , overlap);
+                }
             }
         }
         return ans;
