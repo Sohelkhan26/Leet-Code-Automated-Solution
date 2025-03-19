537c33d038d0b4bf0575668c296c2fcc42d6273f
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-20 01:41:29 +0600

    Time: 0 ms (100.00%) | Memory: 9.3 MB (97.32%) - LeetSync

--- 289-game-of-life/game-of-life.cpp
+++ 289-game-of-life/game-of-life.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
         int n = board.size(), m = board[0].size();
-        vector<vector<int>> ans(n, vector<int>(m));
         
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
@@ -12,17 +11,19 @@ public:
                         if(dx == 0 && dy == 0) continue;  // Skip the current cell
                         int newI = i + dx, newJ = j + dy;
                         if(newI >= 0 && newJ >= 0 && newI < n && newJ < m)
-                            one += board[newI][newJ];
+                            one += (board[newI][newJ] & 1);
                     }
                 }
                 
                 if(board[i][j] == 1) 
-                    ans[i][j] = (one == 2 or one == 3) ? 1 : 0;
+                    board[i][j] |= (one == 2 or one == 3) ? 2 : 0; // using 2 sets the second bit
                 else
-                    ans[i][j] = (one == 3) ? 1 : 0;
+                    board[i][j] |= (one == 3) ? 2 : 0;
             }
         }
-        board = ans;
+        for(int i = 0 ; i < n ; i++)
+            for(int j = 0 ; j < m ; j++)
+                board[i][j] >>= 1;
     }
 };
 
@@ -30,4 +31,6 @@ public:
 Do as said in the problem. 
 Live cell lives on, if there are 2 | 3 live neighbours. Otherwise it dies.
 Dead cell relives if there is exactly 3 live neighbours.
+Remember that the board needs to be updated simultaneously: You cannot update some cells first and then use their updated values to update other cells.
+To update the board inplace, use the 2nd bit store the updated state of each cell. After the whole operation, simply shift every cell 1 bit to the right.
 */
