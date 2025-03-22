ee59d789b2ccbd1d9acc9d61d171e95696d63eb6
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-22 22:19:06 +0600

    Time: 0 ms (100.00%) | Memory: 17.2 MB (61.82%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,14 +1,31 @@
 class Solution {
-public: 
-    map<pair<int,int> , int> dp;
-    int solve(int buy , int i , vector <int> &price){
-        if(i == price.size())
-            return 0;
-        if(dp.find({buy , i}) != dp.end())
-            return dp[{buy , i}];
-        return dp[{buy , i}] = max(buy == INT_MAX ? 0 : price[i] - buy + solve(price[i] , i + 1 , price) , solve(price[i] , i + 1 , price));
-    }
+public:
     int maxProfit(vector<int>& prices) {
-        return solve(INT_MAX , 0 , prices);
+        int n = prices.size() , i = 0 , ans = 0;
+        // while(i < n){
+        //     while(i < n and prices[i + 1] <= prices[i])i++;
+        //     int buy = prices[i];
+        //     while(i < n and prices[i + 1] > prices[i])i++;
+        //     int sell = prices[i];
+        //     ans += sell - buy;
+        // }
+/// Same code below:
+        for(int i = 0 ; i < n - 1 ; i++)
+            if(prices[i + 1] > prices[i])
+                ans += prices[i + 1] - prices[i];
+        return ans;
     }
 };
+
+/*
+prices = [3 , 2 , 5 , 100 , 140 , 1 , 9]
+It is most profitable to buy at last day (when consecutively decreasing 5,4,2 buy at 2), and sell at last day(when consecutively increasing).
+If i buy price[1] = 2 and sell price[4] = 140 , profit = 136
+This is maximum profit i can gain. 
+You can question that , we can buy and sell in between [1,4] and gain more profit. Here comes the important observation: (5-2)+(100-5)+(140-100)=136
+(2-100)+(100-140)=136
+No matter whatever the sequence, profit remains same.
+So the sum of all increases is equal to final sell - initial buy, no matter how many trades you do. This is because addition is associative and distributive—you are just splitting the gain into parts.
+It's because of the telescopic sum property:
+(a-b)+(b-c)+(c-d) = a-d
+*/
