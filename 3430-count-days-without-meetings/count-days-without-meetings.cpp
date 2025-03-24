8f93cc4198ba217f697a39b66e58971f54b0b15c
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-24 15:06:32 +0600

    Time: 83 ms (26.53%) | Memory: 126.5 MB (80.10%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meet) {
-        int n = meet.size() , busy = 0;
+        int free = 0 , prevMeet = 1;
         sort(meet.begin() , meet.end());
-        vector <int> prev = meet[0];
-        for(int i = 1 ; i < n ; i++){
-            if(prev[1] >= meet[i][0]) // overlap of schedules
-                prev[1] = max(meet[i][1] , prev[1]); // Think of how sorting works, value at index 0 is increasing for sure, but not at index 1
-            else{
-                busy += (prev[1] - prev[0] + 1);
-                prev = meet[i];
-            }
+        for(auto &m : meet){
+            free += max(0 , m[0] - prevMeet);
+            prevMeet = max(prevMeet , m[1] + 1);
+            if(prevMeet > days)
+                break;
         }
-        busy += (prev[1] - prev[0] + 1);
-        return days - busy;
+        return free + max(0 , days - prevMeet + 1);
     }
 };
-/*
-Count the number of days there is a meeting, return days - count.
-There could be overlapping meeting schedule. Take care of that.
-*/
