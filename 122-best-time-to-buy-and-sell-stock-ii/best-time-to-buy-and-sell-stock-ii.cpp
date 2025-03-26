f78b474b83657b7f6cf3c57d3c45510076389f3f
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-03-27 02:50:12 +0600

    Time: 189 ms (5.90%) | Memory: 25 MB (5.23%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
+    int dp[100000][2];
+    int solve(int i , vector<int> &price , bool buy){
+        if(i >= price.size())
+            return 0;
+        if(dp[i][buy] != -1)
+            return dp[i][buy];
+        int buyToday = INT_MIN , sellToday = INT_MIN , holdToday = INT_MIN;
+        if(buy)
+            buyToday = solve(i + 1 , price , not buy) - price[i];
+        else sellToday = solve(i + 1 , price , not buy) + price[i];
+        holdToday = solve(i + 1 , price , buy);
+        return dp[i][buy] = max({buyToday , sellToday , holdToday});
+    }
     int maxProfit(vector<int>& prices) {
-        int n = prices.size() , i = 0 , ans = 0;
-        // while(i < n){
-        //     while(i < n and prices[i + 1] <= prices[i])i++;
-        //     int buy = prices[i];
-        //     while(i < n and prices[i + 1] > prices[i])i++;
-        //     int sell = prices[i];
-        //     ans += sell - buy;
-        // }
-/// Same code below:
-        for(int i = 0 ; i < n - 1 ; i++)
-            if(prices[i + 1] > prices[i])
-                ans += prices[i + 1] - prices[i];
-        return ans;
+        memset(dp , -1 , sizeof(dp));
+        return solve(0 , prices , true);
     }
 };
-
-/*
-prices = [3 , 2 , 5 , 100 , 140 , 1 , 9]
-It is most profitable to buy at last day (when consecutively decreasing 5,4,2 buy at 2), and sell at last day(when consecutively increasing).
-If i buy price[1] = 2 and sell price[4] = 140 , profit = 136
-This is maximum profit i can gain. 
-You can question that , we can buy and sell in between [1,4] and gain more profit. Here comes the important observation: (5-2)+(100-5)+(140-100)=136
-(2-100)+(100-140)=136
-No matter whatever the sequence, profit remains same.
-So the sum of all increases is equal to final sell - initial buy, no matter how many trades you do. This is because addition is associative and distributive—you are just splitting the gain into parts.
-It's because of the telescopic sum property:
-(a-b)+(b-c)+(c-d) = a-d
-*/
