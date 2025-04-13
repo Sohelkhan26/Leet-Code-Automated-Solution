52393a95a6b9ec8c383e7d85ada7ae1194e938ff
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-04-13 20:33:28 +0600

    Time: 0 ms (100.00%) | Memory: 8.4 MB (53.69%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        unordered_map <char,int> idx;
+        for(int i = 0 ; i < order.size() ; i++)
+            idx[order[i]] = i;
+        sort(s.begin() , s.end() , [&](char a , char b){
+            return idx[a] < idx[b];
+        });
+        return s;
+    }
+};
