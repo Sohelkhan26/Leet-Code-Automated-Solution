a74d07ca3e2b0c181d8db0679be1cab21394aca5
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-04-17 22:34:43 +0600

    Time: 0 ms (100.00%) | Memory: 45.6 MB (74.00%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        int ans = 0 , prev = 0 , n = values.size();
-        for(int i = 0 ; i < n ; i++){
-            ans = max(ans , prev + values[i] - i);
-            prev = max(prev , values[i] + i);
+        int n = values.size() , ans = 0 , curr = 0;
+        for(int i : values){
+            ans = max(ans , curr + i); // i acts as previous but we don't do nums[i] - i. (j - i) is accounted for by - 1 in the next line. Because we are only concerned with distance
+            curr = max(curr , i) - 1; // - 1 to account for -j
         }
         return ans;
     }
 };
-
-/*
-find such pair such that (nums[i] + i) + (nums[j] - j) is maximized. i != j
-8 1 5 2 6
-8 2 7 5 10 nums[i] + i
-8 0 3 -1 2 nums[j] - j
-Take the biggest value from two resulting array such that i < j 
-As we move forward all spots will be 1 distance further.
-Greedy. Keep track of previous best nums[i] + i and compare with current nums[j] - j
-Almost similar to Best time to buy and sell stock, in this one we keep track of previous max value.
-*/
