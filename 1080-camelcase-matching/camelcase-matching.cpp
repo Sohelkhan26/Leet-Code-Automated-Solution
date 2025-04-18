940cf9f4c8588cb1fa8cd4445e3d172f7b2dcc2b
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-04-18 19:29:16 +0600

    Time: 0 ms (100.00%) | Memory: 9.2 MB (99.64%) - LeetSync

---
+++ 1080-camelcase-matching/camelcase-matching.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+        auto valid = [&](string s){
+            int n = s.size() , m = pattern.size() , i = 0 , j = 0;
+            for(int i = 0 ; i < n ; i++){
+                if(isupper(s[i])){
+                    if(s[i] != pattern[j])
+                        return false;
+                }
+                j += (s[i] == pattern[j]);
+            }
+            return j >= m;
+        };
+        vector <bool> ans;
+        for(string &s : queries)    
+            ans.push_back(valid(s));
+        return ans;
+    }
+};
