dbede6f1cfd1596e2532d1fa9d1d827e4d6a2969
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-04-20 17:13:17 +0600

    Time: 3 ms (29.13%) | Memory: 10.9 MB (49.35%) - LeetSync

--- 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.cpp
+++ 1007-numbers-with-same-consecutive-differences/numbers-with-same-consecutive-differences.cpp
@@ -1,25 +1,24 @@
 class Solution {
-public: 
-    set <int> ans;
-    void solve(int i , int n , int k , int num){
-        if(i == n){
-            if(floor(log10(num)) + 1 == n)
-                ans.insert(num);
+public:
+    vector <int> ans;
+    void solve(int n , int k , int lastDigit , int num){
+        if(n == 0){
+            ans.push_back(num);
             return;
         }
-        int d = num % 10;
-        num *= 10;
-        if(d + k < 10)
-            solve(i + 1 , n , k , num + (d + k));
-        if(d - k >= 0)
-            solve(i + 1 , n , k , num + (d - k));
+        for(int i = 0 ; i < 10 ; i++)
+            if(abs(i - lastDigit) == k)
+                solve(n - 1 , k , i , num * 10 + i);
     }
     vector<int> numsSameConsecDiff(int n, int k) {
-        for(int i = 0 ; i < 10 ; i++)
-            solve(1 , n , k , i);
-        vector <int> res;
-        for(auto it = ans.begin() ; it != ans.end() ; it++)
-            res.push_back(*it);
-        return res;
+        for(int i = 1 ; i < 10 ; i++) // no point in starting from 0 caus it will have n - 1 digits for sure
+            solve(n - 1 , k , i , i);
+        return ans;
     }
 };
+
+/*
+Whatever the last digit may be only one digit in the range [0 , 9] will satisfy
+currDigt + lastDigit <= 9
+At first I thought of this problem in a more complicated manner. If last Digit is greater than k we should subtract , otherwise add etc.
+*/
