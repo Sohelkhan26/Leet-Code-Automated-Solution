bcbfe577aec8684e0e4f936090ac6aafb86579de
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-03 22:09:45 +0600

    Time: 27 ms (56.21%) | Memory: 26.3 MB (44.01%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.size() , a = 0 , b = 0 , ans = n;
-        vector <int> aCount(n) , bCount(n);
-        for(int i = 0 ; i < n ; i++){
-            bCount[i] = b;
-            b += (s[i] == 'b');
-        }
-        for(int i = n - 1 ; i >= 0 ; i--){
-            aCount[i] = a;
-            ans = min(ans , aCount[i] + bCount[i]);
-            a += (s[i] == 'a');
+        stack <char> st;
+        int ans = 0;
+        for(char &c : s){
+            if(not st.empty() and st.top() == 'b' and c == 'a')
+                st.pop() , ans++;
+            else st.push(c);
         }
         return ans;
     }
 };
 
 /*
-Del char from s such that no be occurs before a in s.
-Choose a optimal dividing point such that all left 'b' and all right 'a' is deleted.
-Above approach is not typical suffix and prefix array.
-bCount[i] refers count(b in range[0..i - 1]) index i is not included, meaning if the last char is b, it is not saved in bCount.
+I had a feeling this might be a stack problem.
 */
