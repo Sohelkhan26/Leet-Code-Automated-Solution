76314ff0899946b873e6a79958afb0ed39b0b2a0
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-05 01:02:39 +0600

    Time: 0 ms (100.00%) | Memory: 8.6 MB (56.51%) - LeetSync

--- 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.cpp
+++ 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     long long smallestNumber(long long num) {
-        if(num == 0)
-            return 0;
-        auto solve = [&](long long n){
-            long long ans = 0;
-            bool neg = (n < 0); n = abs(n);
-            vector <int> v;
-            while(n > 0)
-                v.push_back(n % 10) , n /= 10;
-            if(neg)
-                sort(v.rbegin() , v.rend());
-            else{
-                sort(v.begin() , v.end());
-                int i = 0;
-                while(v[i] == 0)
-                    i++;
-                swap(v[i] , v[0]);
-            }
-            for(int &i : v)
-                ans = (ans * 10 + i) , cout << i << ' ';
-            return neg ? ans * -1 : ans;
-        };
-        return solve(num);
+        if(not num)return 0;
+        string s = to_string(abs(num));
+        sort(s.begin() , s.end() , [&](char a , char b){
+            return num > 0 ? a < b : a > b;
+        });
+        swap(s[0] , s[s.find_first_not_of('0')]);
+        return stoll(s) * (num < 0 ? -1 : 1);
     }
 };
