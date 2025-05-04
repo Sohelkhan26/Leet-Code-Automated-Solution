531b4802412b5efd46d489ea1c6db385942b4f30
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-04 22:17:00 +0600

    Time: 7 ms (84.48%) | Memory: 162.3 MB (86.21%) - LeetSync

--- 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
-        vector <long long> pref = {0};
-        for(int &n : nums)
-            pref.push_back(pref.back() + n);
-        long long ans = LLONG_MIN , n = nums.size();
-        for(int s = 0 ; s < k ; s++){
-            long long sum = 0;
-            for(int i = s ; i + k <= n ; i += k){
-                long long subarraySum = pref[i + k] - pref[i];
-                sum = max(sum + subarraySum , subarraySum);
-                ans = max(ans , sum);
-            }
+        vector <long long> prev(k + 1 , 1e18);
+        prev[0] = 0;
+        long long pref = 0 , ans = LLONG_MIN , n = nums.size();
+        for(int i = 0 ; i < n ; i++){
+            pref += nums[i];
+            int len = i + 1 , rem = len % k;
+            // if(rem == 0)
+            //     ans = max(ans , pref); // this is for the initial round. Once prev[0] is updated , it isn't needed.
+            ans = max(ans , pref - prev[rem]);
+            prev[rem] = min(prev[rem] , pref); 
         }
         return ans;
     }
 };
+
 /*
-This approach is great, Uses Kadane. Kadane algo is used to find maximum sum of a subarry. But in this problem one additional constraint. Length of that subarray should be divisible by k.
-This approach starts kadane in range [0..k - 1] because remainder is in this range. Every other subarray will fall in this remainder.
-Prefix sum used to calculate subarray sum in O(1). One padding is used.
-So, instead of writing pref[i + k - 1] - pref[i - 1] and checking for valid i
-pref[i + k] - pref[i] is written. Bigger K less time.
+Same as this: If Same sum repeats , then the sum of in between elements are 0. 
+If remainder length repeats , then current subarray is divisible by k.
+To maximize the sum , we store minimum sum at each length % k.
 */
