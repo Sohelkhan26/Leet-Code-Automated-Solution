6439d4f216df2c9c9f3c73e3c7f6d940e783982c
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-07 16:44:47 +0600

    Time: 0 ms (100.00%) | Memory: 198.9 MB (29.02%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -15,5 +15,7 @@ nums is a permutation [1 .. n]
 every number less than n will have up-to MSB(n). Smaller number won't have a set bit that exceeds the MSB of n. 
 Every bit contributes some, pow(2 , x) where x is the position of the bit. If a same bit contribute twice(in two diff number) then overall contribution will be 0.
 So, xoring max-number "n" with any other number won't result in number greater than n.
+All numbers from 1 to n can be represented with up to k bits.
 
+XOR of 3 numbers (triplets) allows construction of all combinations of those bits.
 */
