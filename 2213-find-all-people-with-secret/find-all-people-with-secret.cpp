523831bdf32eea37dc44564942e0f77f3c51e8b7
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-10 19:47:20 +0600

    Time: 313 ms (34.20%) | Memory: 190.7 MB (38.45%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
@@ -1,43 +1,25 @@
 class Solution {
 public:
-    vector<int>parent;
-    int find(int a){
-        return parent[a] == a ? a : (parent[a] = find(parent[a]));
-    }
-    void connect(int a , int b){
-        parent[find(a)] = find(b);
-    }
-    bool connected(int a , int b){
-        return find(a) == find(b);
-    }
-    void reset(int a){
-        parent[a] = a;
-    }
-    vector<int> findAllPeople(int n, vector<vector<int>>& meet, int firstPerson) { 
-        parent.resize(n);
-        iota(parent.begin() , parent.end() , 0);
-        sort(meet.begin() , meet.end() , [&](auto a , auto b){
-            return a[2] < b[2];
-        }); // person can propagate secret if next meet time is greater 
-        int m = meet.size() , i = 0;
-        connect(0 , firstPerson); // both have same parent, any other node that can be connected to this parent can propagate secret
-        while(i < m){
-            vector <int> secret; // contains group of people with same meeting time
-            int time = meet[i][2];
-            while(i < m and meet[i][2] == time){
-                int a = meet[i][0] , b = meet[i][1];
-                connect(a , b);
-                secret.push_back(a);
-                secret.push_back(b);
-                i++;
+    vector<int> findAllPeople(int n, vector<vector<int>>& meet, int firstPerson) {
+        vector<pair<int, int>> adj[n];
+        meet.push_back({0 , firstPerson , 0}); // subtle but huge
+        for(auto it : meet)
+            adj[it[0]].push_back({it[1] , it[2]}),
+            adj[it[1]].push_back({it[0] , it[2]});
+        vector <int> minTime(n , INT_MAX);
+        minTime[0] = 0;
+        function<void(int , int)> canPass = [&](int curr , int currTime){
+            for(auto &[nei , neiTime] : adj[curr]){ // curr node can pass secret to next node if condition is met
+                if(neiTime >= currTime and minTime[nei] > neiTime){ // curr node has meeting with "nei" at time = "neiTime", if it is held at a later time than currTime(time when curr node received secret) then curr node can pass secret to neighbor node. "minTime[nei] > neiTime" If neighbour node received secret at a earlier time than currNode then no need to pass it twice. So only DFS call when neighbour time is still greater.
+                    minTime[nei] = neiTime;
+                    canPass(nei , neiTime);
+                }
             }
-            for(int node : secret)
-                if(not connected(0 , node)) // if this group is connected to source secret they can propagte otherwise they can't , so reset
-                    reset(node); // Note : 0 , firstPerson belong to same group
-        }
+        };
+        canPass(0 , 0);
         vector <int> ans;
         for(int i = 0 ; i < n ; i++)
-            if(connected(0 , i))
+            if(minTime[i] != INT_MAX)
                 ans.push_back(i);
         return ans;
     }
