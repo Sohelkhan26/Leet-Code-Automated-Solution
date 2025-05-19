a62cb4a79e48ad4a48e964278199721ece7f37ba
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-20 03:55:26 +0600

    Time: 19 ms (73.76%) | Memory: 118.7 MB (96.45%) - LeetSync

--- 2418-minimum-sum-of-squared-difference/minimum-sum-of-squared-difference.cpp
+++ 2418-minimum-sum-of-squared-difference/minimum-sum-of-squared-difference.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     long long minSumSquareDiff(vector<int>& nums1, vector<int>& nums2, int k1, int k2) {
-        long long ans = 0 , n = nums1.size() , K = k1 + k2 , total = 0 , t = 0;
-        priority_queue <int> pq;
+        long long ans = 0 , n = nums1.size() , K = k1 + k2 , extra = 0;
         for(int i = 0 ; i < n ; i++)
-            pq.push(abs(nums1[i] - nums2[i])) , total += abs(nums1[i] - nums2[i]);
-        if(K >= total)
-            return 0;
-        while(K > 0){
-            t = pq.top(); pq.pop();
-            long long reduce = pq.empty() ? K : max(K / n , 1ll);
-            t -= reduce , K -= reduce;
-            pq.push(t);
+            nums1[i] = abs(nums1[i] - nums2[i]);
+        int low = 0 , high = 1e5 , mid , maxDiff = -1;
+        while(low <= high){
+            mid = low + (high - low) / 2;
+            long long op = 0;
+            for(int &num : nums1)
+                op += max(0 , num - mid);
+            if(op <= K){
+                maxDiff = mid;
+                extra = K - op;
+                high = mid - 1;
+            }else low = mid + 1;
         }
-        while(not pq.empty())
-            ans += pow(pq.top() , 2) , pq.pop();
+        for(int &num : nums1){
+            long long val = 0;
+            if(num < maxDiff)
+                val = num;
+            else val = max(0 , maxDiff - (--extra >= 0));
+            ans += val * val;
+        }
         return ans;
     }
 };
-
-/*
-Main observation is combining the two array don't change the answer. 
-Increasing one element in first array is same as decreasing same element in the second array.
-As long as either k1 or k2 remains, change is possible.
-But k1 , k2 <= 10^9 , so +1 , -1 -> TLE.
-We have to reduce square of difference. We can just reduce the biggest difference as long as we have k1 or k2. Right? No, We can't overdo it. Two diff = 5 , 5 , K = 5, changing one element to 0 will yeild ans = 25 , but we try to evenly decrease the two diff, ans = 9 + 4 = 13
-So, we have to make max difference same as next max difference or 1 less. 
-*/
