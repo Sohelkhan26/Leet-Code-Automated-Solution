4f5f3178601dd5f49a7c1fa662064d5b78c09dd7
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-20 21:04:23 +0600

    Time: 12 ms (15.85%) | Memory: 42.3 MB (12.29%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
-    bool canReach(vector<int>& arr, int start) {
-        queue <int> q;
-        q.push(start);
-        int n = arr.size();
-        vector <int> vis(n);
-        while(not q.empty()){
-            int len = q.size();
-            while(len--){
-                int i = q.front(); q.pop();
-                if(arr[i] == 0)
-                    return true;
-                int a = i + arr[i] , b = i - arr[i];
-                auto valid = [&](int a){
-                    return a >= 0 and a < n and not vis[a];
-                };
-                if(valid(a))q.push(a) , vis[a] = true;
-                if(valid(b))q.push(b) , vis[b] = true;
-            }
-        }
+    unordered_set <int> vis;
+    bool canReach(vector<int>& arr, int i) {
+        if(i >= 0 and i < arr.size() and vis.insert(i).second) // insert() returns false if element already exists
+            return arr[i] == 0 or canReach(arr , i + arr[i]) or canReach(arr , i - arr[i]);
         return false;
     }
 };
