e146d4117a40b40da291f29c4c51aa55c98a2b10
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-22 21:30:46 +0600

    Time: 99 ms (40.05%) | Memory: 77.4 MB (32.04%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -3,32 +3,37 @@ public:
     struct node{
         unordered_map <char,node*> child;
         bool end;
-        node(){
-            end = false;
-        }
+        node() : end(false){}
     };
     node *root;
-    bool sub(string s){
+    bool isSubFolder(string s){
         node *curr = root;
         int n = s.size();
         for(int i = 0 ; i < n ; i++){
             char c = s[i];
-            if(c == '/' and curr -> end)
+            if(c == '/' and curr -> end) // there was a previous folder that ended here
+// and this string "s" has suffix folder name, so "s" is a subfolder
                 return true;
             if(curr -> child[c] == nullptr)
                 curr -> child[c] = new node();
             curr = curr -> child[c];
         }
         curr -> end = true;
-        return false;
+        return false; // didn't return earlier so it's new folder
     }
     vector<string> removeSubfolders(vector<string>& folder) {
-        sort(folder.begin() , folder.end());
+        sort(folder.begin() , folder.end()); // sort for processing lexicographically
         root = new node();
         vector <string> ans;
         for(string &s : folder)
-            if(not sub(s))
+            if(not isSubFolder(s))
                 ans.push_back(s);
         return ans;
     }
 };
+
+/*
+string "t" is a subfolder of string "s" if "s" is a prefix of string "t".
+Used trie for better time complexity.
+
+*/
