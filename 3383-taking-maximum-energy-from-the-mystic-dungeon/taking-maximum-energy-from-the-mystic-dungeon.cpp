6dffa77037c2b6a11e29e74087dfe151e0086e2b
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-25 01:02:59 +0600

    Time: 1330 ms (5.26%) | Memory: 176.2 MB (5.26%) - LeetSync

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
-        int n = energy.size();
-        for(int i = n - k - 1 ; i >= 0 ; i--)
-            energy[i] += energy[i + k];
-        return *max_element(energy.begin() , energy.end());
+        int ans = INT_MIN , n = energy.size();
+        vector<int> dp(n , -1);
+        function<int(int)> solve = [&](int i){
+            if(i >= n)
+                return 0;
+            if(dp[i] != -1)
+                return dp[i];
+            int take = energy[i] + solve(i + k);
+            return dp[i] = take;
+        };
+        for(int i = 0 ; i < n ; i++)
+            ans = max(ans , solve(i));
+        return ans;
     }
 };
-
-/*
-Can start from any index, end goal is n - 1 index or higher.
-Starting from any index is important. We can start from n - k and max energy is energy[i] + energy[i + k]
-or start from n - 2 * k , then ans = sum([n - 2 * k , n])
-for(int i = 0 ; i < n - k ; i++)
-    energy[i] += energy[i + k];
-If we start the loop from the beginning of the array, we can't simulate this process, It double counts for starting points and indices that come after it.
-If we start from i , i + k accumulate energy[i] and again when after some time in loop we reach i + k, it propagates energy[i] and energy[i + k] basically two starting point (i and i + k).
-*/
