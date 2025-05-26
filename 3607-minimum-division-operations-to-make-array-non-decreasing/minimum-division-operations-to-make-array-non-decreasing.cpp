c8728b551a4194220e296128d73c3b13da36517c
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-27 01:00:39 +0600

    Time: 287 ms (47.23%) | Memory: 143 MB (54.47%) - LeetSync

--- 3607-minimum-division-operations-to-make-array-non-decreasing/minimum-division-operations-to-make-array-non-decreasing.cpp
+++ 3607-minimum-division-operations-to-make-array-non-decreasing/minimum-division-operations-to-make-array-non-decreasing.cpp
@@ -1,24 +1,28 @@
+
 class Solution {
-private:
 public:
     int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        int ans = 0;
-        for(int i = n-2; i>=0; i--){
-            if(nums[i] > nums[i+1]){
-                
-                for(int j = 2; j*j <= nums[i]; j++){
-                    if(nums[i]%j == 0){
-                        nums[i] = j;
-                        ans++;
-                        break;
-                    }
-                }
-
-                if(nums[i]>nums[i+1]) return -1;
-            }
+        auto div = [&](int n){
+            for(int i = 2 ; i < n ; i++)
+                if(n % i == 0)
+                    return i;
+            return n;
+        };
+        int n = nums.size() , ans = 0;
+        for(int i = n - 1 ; i > 0 ; i--){
+            if(nums[i - 1] > nums[i])
+                nums[i - 1] = div(nums[i - 1]) , ans++;
+            if(nums[i] < nums[i - 1])
+                return -1;
         }
-
         return ans;
     }
 };
+
+/*
+If you do as the problem asks, it gets accepted. 
+Proper divisor of a number 'n' is a divisor that is strictly less than n
+But the logic is : dividing a number by it's proper divisor
+results in prime number. Since it's a prime number atmost 1 operation
+can be performed on a number. I still don't get the logic.
+*/
