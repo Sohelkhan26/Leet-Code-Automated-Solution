2a257665aeb7b7a6aa8968e8a09fa478e1b482a2
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-27 15:48:36 +0600

    Time: 2329 ms (11.72%) | Memory: 466.7 MB (6.28%) - LeetSync

--- 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
+++ 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
@@ -2,15 +2,23 @@ class Solution {
 public:
     int maxTotalReward(vector<int>& reward) {
         sort(reward.begin() , reward.end());
-        unordered_set <int> ans = {0};
+        set <int> ans = {0};
         for(int &r : reward){
-            unordered_set <int> extendAns;
+            set <int> extendAns;
             for(const int &prev : ans){
                 if(r > prev)
                     extendAns.insert(r + prev);
             }
             ans.insert(extendAns.begin() , extendAns.end());
         }
-        return *max_element(ans.begin() , ans.end());
+        return *prev(ans.end());
     }
 };
+
+/*
+Really cool solution. First thought of greedy. Gradually pick smaller.
+That doesn't gaurantee maximum answer. To get the optimal answer, we need to use DP , pick or not pick type. But the total-reward is gonna pose a problem.
+What could be the max-total-reward? Probably some type of complext math is involved. Instead we go with this approach, Some other problem was solved by this method before. 
+First set contains all possible sum of subsequence of rewards[i]. Now for each new reward we try to extend our answer by including it if reward[i] is greater than our current answer. It's same as DP but instead of keeping track of index and current score/answer, we keep only answer.
+unordered_set used for better efficiency.
+*/
