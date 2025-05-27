067ca19dddb7351341618055170e857939c5b15c
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-05-27 16:27:50 +0600

    Time: 209 ms (19.00%) | Memory: 30.8 MB (76.70%) - LeetSync

--- 3654-minimum-array-sum/minimum-array-sum.cpp
+++ 3654-minimum-array-sum/minimum-array-sum.cpp
@@ -6,14 +6,14 @@ public:
             return 0;
         if(dp[i][a][b] != -1)
             return dp[i][a][b];
-        int half = ceil(nums[i] / 2.0) , one = Max , two = Max , both1 = Max , both2 = Max , none = Max;
+        int half = (nums[i] + 1) / 2.0 , one = Max , two = Max , both1 = Max , both2 = Max , none = Max;
         if(a)
             one = solve(i + 1 , a - 1 , b , nums);
         if(b and nums[i] >= k)
             two = solve(i + 1 , a , b - 1 , nums);
-        if(a and b and half >= k)
+        if(a and b and half >= k) // op1 then op2
             both1 = solve(i + 1 , a - 1 , b - 1 , nums);
-        if(a and b and nums[i] >= k)
+        if(a and b and nums[i] >= k) // op2 then op1
             both2 = solve(i + 1 , a - 1 , b - 1 , nums);
         none = solve(i + 1 , a , b , nums);
         return dp[i][a][b] = min({one + half , two + nums[i] - k , both1 + half - k , both2 + (nums[i] - k + 1) / 2 , none + nums[i]});
@@ -24,3 +24,15 @@ public:
         return solve(0 , op1 , op2 , nums);
     }
 };
+
+/*
+Pretty awesome and annoying too. Constraints are small so easily Brute Force in other words DP.
+For every number we can either do:
+1. operation 1
+2. operation 2
+3. Both operation
+4. Skip it.
+There is a slight edge case for both operation. 2nd operation can be only applied if nums[i] >= k
+After 1st operation half = (nums[i] + 1) / 2 may be < k , then we can't do 2nd operation.
+So, two case for both operation: half >= k or nums[i] >= k 
+*/
