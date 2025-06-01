e63392e8b0293dad71a7678dc11cf01ecb67aa60
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-02 05:20:46 +0600

    Time: 0 ms (100.00%) | Memory: 22.6 MB (23.02%) - LeetSync

---
+++ 3234-double-modular-exponentiation/double-modular-exponentiation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxPow(int a , int b , int mod){
+        if(b == 0)
+            return 1;
+        int ans = maxPow(a , b / 2 , mod) % mod;
+        if(b % 2)
+            return (1ll * ans * ans * a) % mod;
+        else return (1ll * ans * ans) % mod;
+    } 
+    vector<int> getGoodIndices(vector<vector<int>>& variables, int target) {
+        vector <int> ans;
+        for(int i = 0 ; i < variables.size() ; i++){
+            auto it = variables[i];
+            int a = it[0] , b = it[1] , c = it[2] , m = it[3];
+            int res = maxPow(a , b , 10);
+            res = maxPow(res , c , m);
+            if(res == target)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
