fbf3e61e7f33ac1ec5c3f474766c9c3e7c380e17
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-17 02:02:26 +0600

    Time: 39 ms (78.29%) | Memory: 45.4 MB (58.43%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,14 +2,20 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map <int,int> prev;
-        int n = nums.size() , ans = 0 , sum = 0;
-        prev[0] = 1;
+        int n = nums.size() , ans = 0 , prefix = 0;
+        prev[0] = 1; // this is used for the first prefix sum = k 
         for(int i = 0 ; i < n ; i++){
-            sum += nums[i];
-            if(prev.contains(sum - k))
-                ans += prev[sum - k];
-            prev[sum]++;
+            prefix += nums[i];
+            if(prev.contains(prefix - k))
+                ans += prev[prefix - k];
+            prev[prefix]++;
         }
         return ans;
     }
 };
+
+/*
+Intuition is pretty straightforward.
+If we encounter the same prefix-sum later in the array, that means, middle subarray sum is 0
+That fact is used here. If we countered "prefix - k" sum in the past, that means the middle subarray sum = prefx(current prefix sum) - (prefix - k) = k 
+*/
