96a67921217c17c7e0b7c30e4831d5e207f4e3a8
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-18 04:01:26 +0600

    Time: 239 ms (32.92%) | Memory: 231.9 MB (35.08%) - LeetSync

--- 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.cpp
+++ 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int longestEqualSubarray(vector<int>& nums, int k) {
-        unordered_map <int,vector<int>> mp;
+        unordered_map <int,vector<int>> mp; // nums[i] -> i
         for(int i = 0 ; i < nums.size() ; i++)
             mp[nums[i]].push_back(i);
         int ans = 1;
@@ -16,3 +16,12 @@ public:
         return ans;
     }
 };
+/*
+4 -> 1,3,5,9,13 , k = 3 or whatever
+the sum of gap in-between indices can be at most k (k element can be deleted)
+If sum-gap exceeds k, shrink window.
+idx[right] - idx[left] + 1 -> operation needed
+But, we have (right - left + 1) -> elements present.
+So, operation = (idx[right] - idx[left] + 1) - (right - left + 1)
+              = (idx[right] - idx[left]) - (right - left) is the desired window.
+*/
