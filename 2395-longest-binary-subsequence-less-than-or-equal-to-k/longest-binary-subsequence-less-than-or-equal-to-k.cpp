f55a5d1916e15b70dcfb242108b833b9645ad0b1
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-26 16:04:27 +0600

    Time: 0 ms (100.00%) | Memory: 9.6 MB (50.79%) - LeetSync

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-    int k;
-    map <pair<int,int> , int> dp;
-    int solve(string &sub , int i , string &s){
-        if(i == s.size())
-            return not sub.empty() and stoi(sub , nullptr , 2) <= k ? 0 : -1e4;
-        if(not sub.empty() and stoi(sub , nullptr , 2) > k)
-            return -1e4;
-        int key = stoi(sub , nullptr , 2);
-        if(dp.contains({key , i}))
-            return dp[{key , i}];
-        int take = 0 , skip = solve(sub , i + 1 , s);
-        sub.push_back(s[i]);
-        take = 1 + solve(sub , i + 1 , s);
-        sub.pop_back();
-        return dp[{key , i}] = max(take , skip);
-    }
     int longestSubsequence(string s, int K) {
         int ans = s.size() , bit = 0;
         for(int i = s.size() - 1 ; i >= 0 ; i-- , bit++){
@@ -37,5 +21,14 @@ return longest subseq (convereted to decimal) is <= k
 
 subseq can contains leading 0(all zero possible? yeah , 1 <= k <= 1e9)
 all 0 subseq always possible.
-subseq can be only 32 length long. 
+subseq can be only 32 length long. There could be more than 32 zero in MSB, that don't really matter, I don't know.
+
+Greedy solution works:
+Hard to be exact. I just had a hunch.
+k in binary has these set bits : 0..0011111
+s =                     00000111110..010001
+we can pick 5 consecuitve ones or be greedy and take the first 2 ones
+but in order to take the 5 ones, we have to discard 010001 from beginining
+Even if there were no 0 in the middle this can be shown, to gain more 'x' later, we have to discard a length of at least 'x' from the beginning. 
+It's not even guaranteed to gain 'x' (exactly count of set bits in k) by discarding a len >= x from beginning. 
 */
