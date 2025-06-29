4b687692421d926fc3202fa9ed6e29099e979cb5
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-30 02:29:20 +0600

    Time: 3 ms (73.97%) | Memory: 10.7 MB (44.27%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int numTilePossibilities(string tiles) {
-        vector <int> count(26);
-        for(char &c : tiles)
+    int fact[8] = { 1, 1, 2, 6, 24, 120, 720, 5040 };
+    unordered_set <string> seen;
+    int calc(string sub){
+        int n = sub.size() , count[26] = {};
+        for(char &c : sub)
             count[c - 'A']++;
-        function<int()> dfs = [&](){
-            int total = 0;
-            for(int i = 0 ; i < 26 ; i++){
-                if(count[i] == 0)
-                    continue;
-                total++;
-                count[i]--;
-                total += dfs();
-                count[i]++;
-            }
-            return total;
-        };
-        return dfs();
+        int ans = fact[n];
+        for(int i = 0 ; i < 26 ; i++)
+            ans /= fact[count[i]];
+        return ans;
+    }
+    int dfs(int i , string sub , string &s){
+        if(i == s.size())
+            return seen.insert(sub).second ? calc(sub) : 0;
+          
+        return dfs(i + 1 , sub , s) + dfs(i + 1 , sub + s[i] , s);
+    } 
+    int numTilePossibilities(string tiles) {
+        sort(tiles.begin() , tiles.end());
+        return dfs(0 , "" , tiles) - 1; // -1 for empty subseq
     }
 };
