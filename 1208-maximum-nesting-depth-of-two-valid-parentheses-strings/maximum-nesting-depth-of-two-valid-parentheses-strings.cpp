7b14992ecd65742fc1e32773f85494e60ff48ed5
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-06-30 17:53:51 +0600

    Time: 0 ms (100.00%) | Memory: 10.4 MB (95.56%) - LeetSync

---
+++ 1208-maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> maxDepthAfterSplit(string s) {
+        int n = s.size() , group = 0; 
+        vector <int> ans(n);
+        for(int i = 0 ; i < n ; i++){
+            char c = s[i];
+            if(c == '(')
+                ans[i] = ++group % 2;
+            else
+                ans[i] = group-- % 2;
+        }
+        return ans;
+    }
+};
+
+/*
+Such a long description.
+Given a string , split it into two disjoint set such that
+max(depth(a) , depth(b)) is minimum
+
+a and b must be balanced. In short: there exists 1 ) for each (
+depth("") = 0
+depth(a + b) = max(depth(a) , depth(b))
+depth("(a)") = 1 + depth(a)
+
+Note: input is balanced
+
+ans[i] = 0 if it's in a otherwise 1
+I think it's a trick problem.
+order like this : ()()() try to reduce nested brackets like this : ((()))
+It's always possible to arrange a = all () and b = all ()
+if nested input: ((())) each new layer need to go to different group. a -> b -> a
+*/
