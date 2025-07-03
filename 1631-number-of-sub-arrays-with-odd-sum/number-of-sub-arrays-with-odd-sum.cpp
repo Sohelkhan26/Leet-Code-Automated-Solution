abbe5ee574cc612fd3e204f98ea990fa59678036
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-03 18:34:05 +0600

    Time: 14 ms (15.53%) | Memory: 112 MB (78.49%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
-        int prev[2] = {0 , 0} , prefix = 0 , n = arr.size() , ans = 0 , mod = 1e9 + 7;
+        int seen[2] = {} , ans = 0 , mod = 1e9 + 7 , prefix = 0;
         for(int &x : arr){
             prefix += x;
             int rem = prefix % 2;
-            if(rem == 1)
+            if(rem) // the whole subarray is valid
                 ans++;
-            ans = (ans + prev[1 - rem]) % mod;
-            prev[rem]++;
+            ans = (ans + seen[1 - rem]) % mod; // x parity subarray will create valid subarray with (not x) parity
+            seen[rem]++;
         }
         return ans;
     }
 };
+
+/*
+subarray with odd sum -> valid
+count valid subarray % mod
+
+Obviously a prefix sum sliding window. Maybe I tried this problem before, timer counter was 35 minutes.
+Every prefix sum with odd parity will create a valid subarray, with every previous prefix with even parity. And vice versa.
+Only parity of prefix sum is required.
+*/
