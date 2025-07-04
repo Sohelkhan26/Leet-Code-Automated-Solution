4166dc1303d5861941fede69fcfef647dfd5e277
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-05 01:40:45 +0600

    Time: 115 ms (16.60%) | Memory: 78.5 MB (10.81%) - LeetSync

--- 1946-minimum-absolute-sum-difference/minimum-absolute-sum-difference.cpp
+++ 1946-minimum-absolute-sum-difference/minimum-absolute-sum-difference.cpp
@@ -3,21 +3,18 @@ public:
     const int mod = 1e9 + 7;
     int minAbsoluteSumDiff(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
-        long long sum = 0 , ans = 0;
-        set <int> st;
-        for(int i = 0 ; i < n ; i++)
-            sum = (sum + abs(nums1[i] - nums2[i]))  , st.insert(nums1[i]);
-        ans = sum;
+        long long total = 0 , reduce = 0;
+        set <int> st(nums1.begin() , nums1.end());
         for(int i = 0 ; i < n ; i++){
-            sum -= abs(nums1[i] - nums2[i]);
+            long long currDiff = abs(nums1[i] - nums2[i]);
+            total += currDiff;
             auto it = st.lower_bound(nums2[i]);
             if(it != st.end())
-                ans = min(ans , (sum + abs(*it - nums2[i])) );
+                reduce = max(reduce , currDiff - abs(*it - nums2[i]));
             if(it != st.begin())
-                ans = min(ans , (sum + abs(*prev(it) - nums2[i])) );
-            sum += abs(nums1[i] - nums2[i]);
+                reduce = max(reduce , currDiff - abs(*prev(it) - nums2[i]));
         }
-        return ans % mod;
+        return (total - reduce) % mod;
     }
 };
 
@@ -27,4 +24,7 @@ For each element greedily replace it with another element and recalculate answer
 Return the min. 
 Just a hunch. could be wrong.
 choose greedily how? binary search over nums1?
+Works just fine. Awesome. 
+We want to reduce the absolute sum difference. So, nums1[i] and nums2[i] should be as close as possible to make difference closer to 0
+So, lower_bound(nums2[i]) works. Since we can do 1 operation, track the maximum reduce
 */
