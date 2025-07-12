28bee95604ffc27f6e9e2ae6a6da1d2e4bbc7a16
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-12 11:20:49 +0600

    Time: 30 ms (94.88%) | Memory: 47.4 MB (96.01%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        int n = nums.size();
-        auto solve = [&](int K){
-            int ans = 0;
-            unordered_map <int,int> window;
-            for(int left = 0 , right = 0 ; right < n ; right++){
-                window[nums[right]]++;
-                while(window.size() > K){
-                    if(--window[nums[left]] == 0)
-                        window.erase(window.find(nums[left]));
-                    left++;
-                }
-                ans += (right - left + 1);
+        int n = nums.size() , ans = 0 , valid = 0;
+        unordered_map <int,int> window;
+        for(int left = 0 , right = 0 ; right < n ; right++){
+            if(++window[nums[right]] == 1)
+                k--;
+            if(k < 0){
+                if(--window[nums[left++]] == 0)
+                    k++;
+                valid = 0;
             }
-            return ans;
-        };
-        return solve(k) - solve(k - 1);
+            if(k == 0){
+                while(window[nums[left]] > 1)
+                    --window[nums[left++]] , valid++;
+                ans += (valid + 1); // 1 for smallest window that has k distinct elements
+            }
+        }
+        return ans;
     }
 };
