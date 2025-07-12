6662750a29b847eb7a144bfddec5e45db59b4cca
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-12 11:57:34 +0600

    Time: 37 ms (94.37%) | Memory: 47.7 MB (95.15%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
         unordered_map<int, int> window; 
-        int leftFar = 0 , leftNear = 0 , ans = 0;
+        int leftFar = 0 , leftNear = 0 , ans = 0 , n = nums.size();
 
-        for (int i = 0; i < nums.size(); i++) {
-            window[nums[i]]++;
 
-            while (window.size() > k) { 
-                if (--window[nums[leftNear]] == 0)
+        for(int right = 0 ; right < n ; right++){
+            window[nums[right]]++;
+            while(window.size() > k){
+                if(--window[nums[leftNear]] == 0)
                     window.erase(nums[leftNear]);
                 leftNear++;
                 leftFar = leftNear;
             }
-
-            while (window[nums[leftNear]] > 1) 
-                window[nums[leftNear++]]--;
-
+            while(window[nums[leftNear]] > 1)
+                --window[nums[leftNear++]];
             if(window.size() == k)
-                ans += leftNear - leftFar + 1;
+                ans += (leftNear - leftFar + 1);
         }
 
         return ans;
