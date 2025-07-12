aed462bc788f61652f83c8dc7e2abcbb6a6e34cb
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-12 19:32:42 +0600

    Time: 343 ms (13.64%) | Memory: 50.7 MB (41.98%) - LeetSync

---
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public: 
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        int n = grid.size() , m = grid[0].size() , time = 0;
+        queue <array<int,3>> q;
+        int dir[5] = {1 , 0 , -1 , 0 , 1};
+        auto valid = [&](int x , int y){
+            return min(x , y) >= 0 and x < n and y < m;
+        };
+        q.push({0 , 0 , k});
+        set <array<int,3>> vis;
+        vis.insert({0 , 0 , k});
+
+        while(not q.empty()){
+            int len = q.size();
+            while(len--){
+                auto it = q.front(); q.pop();
+                int x = it[0] , y = it[1] , K = it[2];
+
+                if(x == n - 1 and y == m - 1)
+                    return time;
+
+                for(int k = 0 ; k < 4 ; k++){
+                    int newX = x + dir[k] , newY = y + dir[k + 1];
+                    if(valid(newX , newY)){
+                        int newK = K - (grid[newX][newY] == 1);
+                        if(newK < 0 or vis.contains({newX , newY , newK}))
+                            continue;
+                        q.push({newX , newY , newK});
+                        vis.insert({newX , newY , newK});
+                    }
+                }
+            }
+            time++;
+        }
+        return -1;
+    }
+};
