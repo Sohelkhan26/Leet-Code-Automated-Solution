87eb0a2c996c4bd04e33e60828b3f117969bc686
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-15 00:56:23 +0600

    Time: 1 ms (53.31%) | Memory: 92.9 MB (48.65%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -10,3 +10,12 @@ public:
         return ans;
     }
 };
+
+/*
+It's a trick question. Don't think of it like a graph problem to clear i'th computer we should come from j'th comuter such that j < i and complexity[j] < complexity[i].
+To open i'th computer we have must have an earlier j'th computer whose complexity is less than i'th computer.
+I thought if 1,14,3
+if 14 is unlocked we can't then unlock 3 because 14 > 3
+But we can unlock 3 cause we have 1 , we don't have to follow 14 -> 3
+we can go from earlier computer too. 1 -> 3
+*/
