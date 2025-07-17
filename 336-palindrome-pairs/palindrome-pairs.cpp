9a2b3b35471be70cf2b8516ca61976f6bce19a9a
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-17 12:37:31 +0600

    Time: 630 ms (79.02%) | Memory: 598.9 MB (29.02%) - LeetSync

--- 336-palindrome-pairs/palindrome-pairs.cpp
+++ 336-palindrome-pairs/palindrome-pairs.cpp
@@ -1,66 +1,53 @@
 class Solution {
 public:
-    struct Node{
-        bool isEnd = false;
+    struct Node {
         Node* child[26] = {};
-        int index = -1; // ending of word. int instead of bool to avoid pairing with itself. to account for i != j
-        vector<int> palindromes; // stores only palindromic prefix of words
-    } *root , *curr;
+        int index = -1;
+        vector<int> palindromes; // store indices of palindromic suffix
+    } *root = new Node();
 
-    bool palindrome(string &s , int i , int j){
+    bool palindrome(const string& s, int i, int j) {
         while(i <= j)
             if(s[i++] != s[j--])
                 return false;
         return true;
     }
- 
-    void add(string &s , int idx){
-        curr = root;
-        for(int i = s.size() - 1 ; i >= 0 ; i--){ // we are inserting reverse of words i.e: suffixes of words
-            if(palindrome(s , 0 , i)) // prefix is palindrome
-                curr -> palindromes.push_back(idx);
+
+    void add(const string &s , int idx){
+        Node* curr = root;
+        int n = s.size();
+        for(int i = 0 ; i < n ; i++){
             int j = s[i] - 'a';
+            if(palindrome(s , i , n - 1)) // store variable length suffix palindrome
+                curr -> palindromes.push_back(idx);
             if(curr -> child[j] == nullptr)
                 curr -> child[j] = new Node();
             curr = curr -> child[j];
         }
-        curr -> index = idx; // end of word
-        curr -> palindromes.push_back(idx); // empty string is palindrome too. when prefix and suffix of two different words match i.e : abcd , dcba or one string is empty i.e: a , ""
+        curr -> index = idx; // mark end of word
+        curr -> palindromes.push_back(idx); // empty suffix palindrome present at idx
     }
+
     vector<vector<int>> palindromePairs(vector<string>& words) {
-        root = new Node();
+        for (int i = 0; i < words.size(); i++)
+            add(words[i], i);
+
         vector<vector<int>> ans;
-        int n = words.size();
-        for(int i = 0 ; i < n ; i++)
-            add(words[i] , i);
-        for(int i = 0 ; i < n ; i++){
-            curr = root;
-            int m = words[i].size();
-/*This for loops finds palindrome that have words[i] as prefix and some other word as suffix and there are some characters left in words[i] even after matching with some other word. words[i] is bigger. i.e: words[i] = llss other word = lls */
-            for(int j = 0 ; j < m and curr ; j++){ // for each words traverse from front
-                if(curr -> index != -1 and curr -> index != i and palindrome(words[i] , j , m - 1))
-                    ans.push_back({i , curr -> index}); // we found words[i][0..j] to some other words suffix
-                curr = curr -> child[words[i][j] - 'a']; // and rest of words[i] is palindrome too.
+        for(int i = 0 ; i < words.size() ; i++){
+            string &word = words[i];
+            Node* curr = root;
+            int n = word.size();
+            for(int j = n - 1 ; j >= 0 and curr ; j--){
+                if(curr -> index != -1 and curr -> index != i and palindrome(word , 0 , j)) // current words suffix matches some other whole words prefix, current word is bigger, so check if rest of word(prefix of current word) is palindrome too. i.e: words[i] = llss other word = lls
+                    ans.push_back({curr -> index , i});
+                curr = curr -> child[word[j] - 'a'];
             }
-            if(curr == nullptr)
-                continue;
-            for(int j : curr -> palindromes) // if whole words[i] matches with some other words suffix and that other word is still bigger. Now if rest of that other word is palindrome too, we found another valid pair. i.e: words[i] = lls other word = llsss
-                if(i != j)
-                    ans.push_back({i , j});
+            if(curr) 
+                for(int j : curr -> palindromes) // current word smaller and some other word is bigger. i.e: words[i] = lls other word = llsss. rest of the other word is also a palindrome, that's why it's stored here.
+                    if(j != i)
+                        ans.push_back({j , i});
         }
+
         return ans;
     }
 };
-
-/*
-Am i overcomplicating things or this problem is harder than most?
-bidirectional pairs are not always possible.
-abc -> ba but not ba -> abc
-But sssll -> lls and lls -> sssll
-How to distinguish between the two?
-I think Trie is needed. Or hashing?
-And to make things even more complicated input can contains empty strings.
-As if things were not complicated enough.
-abcd -> dcba 
-dcba -> abcd
-*/
