f71a27ea1096ac3e7d2c1ffdc30c63f64e2e9980
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-20 01:39:23 +0600

    Time: 803 ms (35.01%) | Memory: 285.4 MB (53.62%) - LeetSync

--- 3927-minimum-cost-path-with-alternating-directions-ii/minimum-cost-path-with-alternating-directions-ii.cpp
+++ 3927-minimum-cost-path-with-alternating-directions-ii/minimum-cost-path-with-alternating-directions-ii.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    vector<vector<long long>> dp; // maybe 3D needed
-    
-    int n , m;
-    long long solve(int i , int j , bool parity , vector<vector<int>> &cost){ 
-        if(i == n or j == m)
-            return 1e14;
-        if(i == n - 1 and j == m - 1)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        long long extra = (parity == 0 ? cost[i][j] : 0);
-        long long right = 1ll * (j + 2) * (i + 1) + solve(i , j + 1 , 0 , cost);
-        long long down = 1ll * (i + 2) * (j + 1) + solve(i + 1 , j , 0 , cost);
-        return dp[i][j] = min(down , right) + extra;
-    }
-    long long minCost(int row , int col , vector<vector<int>>& waitCost) {
-        n = row , m = col;
-        dp = vector<vector<long long>> (n , vector<long long> (m , -1));
-        return 1 + solve(0 , 0 , 1 , waitCost);
+    long long minCost(int row , int col, vector<vector<int>>& waitCost) {
+        vector<vector<long long>> dist(row , vector<long long> (col , LLONG_MAX));
+        dist[0][0] = 1;
+        priority_queue <tuple <long long,int,int> , vector<tuple<long long , int , int>> , greater<> > pq;
+        pq.push({1 , 0 , 0});
+
+        auto valid = [&](int x , int y){
+            return x < row and y < col;
+        };
+
+        while(not pq.empty()){
+            auto [cost , x , y] = pq.top(); pq.pop();
+            if(x == row - 1 and y == col - 1)
+                return cost - waitCost[row - 1][col - 1];
+            if(dist[x][y] < cost)continue;
+            for(auto &[dx , dy] : vector<pair<int,int>> {{1 , 0} , {0 , 1}}){
+                int newX = x + dx , newY = y + dy;
+                if(valid(newX , newY)){
+                    long long newCost = cost + 1ll * (newX + 1) * (newY + 1) + waitCost[newX][newY];
+                    if(dist[newX][newY] > newCost)
+                        pq.push({newCost , newX , newY}) , dist[newX][newY] = newCost;
+                }
+            }
+        }
+        
+        return -1;
     }
 };
