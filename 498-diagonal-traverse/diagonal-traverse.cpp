b8046e4f4f36a00cc4dcc34025b0cb72e20f770a
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2025-07-23 21:56:36 +0600

    Time: 0 ms (100.00%) | Memory: 22.4 MB (92.19%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        map <int,vector<int>> diags;
-        int n = mat.size() , m = mat[0].size();
-
-        for(int i = 0 ; i < n ; i++)
-            for(int j = 0 ; j < m ; j++)
-                diags[i + j].push_back(mat[i][j]);
-
-        bool flip = true;
-        vector <int> ans;
-
-        for(auto &[ _ , v ] : diags){
-            if(flip)
-                reverse(v.begin() , v.end()) , flip = false;
-            else flip = true;
-            ans.insert(ans.end() , v.begin() , v.end());
+        int n = mat.size() , m = mat[0].size() , row = 0 , col = 0;
+        vector<int> ans(n * m);
+        for(int pos = 0 ; pos < m * n ; pos++){
+            ans[pos] = mat[row][col];
+            if((row + col) % 2 == 0){ // (row + col) == constant for  left-right upward diagonals. Based on it's parity we have to decide if we should go up or down.
+                if(col == m - 1) // col reaches end of mat[i]
+                    row++; // start from new diagonal starting under it
+                else if(row == 0) // end of diagonal, start from next diagonal at col + 1 , go down
+                    col++;
+                else // diagonal not finished yet keep going
+                    col++ , row--;
+            }else{ // go down diagonally
+                if(row == n - 1)
+                    col++;
+                else if(col == 0)
+                    row++;
+                else
+                    row++ , col--;
+            }
         }
         return ans;
     }
