0c92f48d9c926dc1c58c257d8b2eac5911e1970b
Sohelkhan26 <129702638+Sohelkhan26@users.noreply.github.com>
2026-01-21 21:47:01 +0600

    Time: 8 ms (83.44%) | Memory: 32.8 MB (38.63%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> pref;
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
-        int n = mat.size(), m = mat[0].size();
-        pref.resize(n + 1 , vector<int> (m + 1, 0));
+        int n = mat.size() , m = mat[0].size();
+        vector<vector<int>> pref(n + 1 , vector<int> (m + 1 , 0));
+        for(int i = 1 ; i <= n ; i++) // standard code for calculating 2D prefix sum
+            for(int j = 1 ; j <= m ; j++) // to account for offset by one , use mat[i-1][j-1] instead of mat[i][j]
+                pref[i][j] = mat[i - 1][j - 1] + pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                pref[i][j] = mat[i - 1][j - 1] + pref[i - 1][j] +
-                             pref[i][j - 1] - pref[i - 1][j - 1];
-            }
-        }
-
-        auto check = [&](int len) { 
-            for (int i = 0; i + len <= n; i++) {
-                for (int j = 0; j + len <= m; j++) {
-                    if (pref[i + len][j + len] - pref[i + len][j] - pref[i][j + len] + pref[i][j] <= threshold)
-                        return true;
-                }
-            }
+        int low = 0 , high = min(n , m) , mid , ans;
+        auto compare = [&](int len){
+            for(int i = len ; i <= n ; i++)for(int j = len ; j <= m ; j++) // pref[i][j] contains 2D sum up until this co-ordinate. Can be related with the previous formula. we want sum of the square.
+                if(pref[i][j] - pref[i - len][j] - pref[i][j - len] + pref[i - len][j - len] <= threshold)
+                    return true; // pref[i - len][j - len] was subtracted twice so adding once.
             return false;
         };
-
-        int low = 0, high = min(n, m), ans = 0;
-        while (low <= high) {
-            int len = low + (high - low) / 2;
-            if (check(len)) {
-                ans = len;
-                low = len + 1;
-            } else {
-                high = len - 1;
-            }
+        while(low <= high){ // standard BS
+            mid = low + (high - low) / 2;
+            if(compare(mid)) // Have to find highest square side
+                ans = mid , low = mid + 1;
+            else high = mid - 1;
         }
         return ans;
     }
 };
+// To know more about 2D prefix sum , refer to this article: https://usaco.guide/silver/more-prefix-sums?lang=cpp
